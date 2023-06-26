c4552f08eccd192ad562ee61b6bb75e621455cee
vito-pizzulli <vito.pizzulli.webdev@outlook.it>
2023-06-26 15:30:30 +0200

    header content put in navbar by mistake, all moved to AppHeader, added comments for better understanding

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,9 +1,42 @@
 <template>
     <header>
+        <!-- Navbar Component -->
         <Navbar
             :sitePages="sitePages"
             :departments="departments"
         />
+
+        <!-- Central info section -->
+        <div id="header-info">
+
+            <!-- Left-side section with title -->
+            <div class="title">
+                <h1>Caring <span>For Life</span></h1>
+                <p>Kind words can be short and easy to speak, but their echoes are truly endless. Avada Health focuses on you as if it was our own family.</p>
+                <button>Learn more</button>
+            </div>
+
+            <!-- Right-side section with office hours -->
+            <div class="office-hours">
+                <span>Office Hours</span>
+                <hr>
+                <table>
+                    <tr>
+                        <td>Monday - Friday</td>
+                        <td>8:00 - 5:00</td>
+                    </tr>
+                    <tr>
+                        <td>Saturday</td>
+                        <td>9:00 - 5:00</td>
+                    </tr>
+                    <tr>
+                        <td>Sunday</td>
+                        <td>11:00 - 4:00</td>
+                    </tr>
+                </table>
+                <span>+44(0) 1865 339665</span>
+            </div>
+        </div>
     </header>
 </template>
 
@@ -24,10 +57,71 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+    @use '../styles/partials/variables' as *;
+    @use '../styles/partials/mixins' as *;
     
     header {
         background-image: url('../assets/img/header-image-homepage.jpg');
         background-size: 120%;
         background-position: top;
+
+        div#header-info {
+            @include flex(row, center, center, no-wrap);
+            width: 65%;
+            margin: auto;
+            padding: 12rem 0;
+            color: $white;
+            transform: translateY(-8%);
+            
+            div.title {
+                width: 70%;
+
+                h1 {
+                    font-size: 5.5rem;
+                    font-weight: 500;
+
+                    span {
+                        color: $teal;
+                    }
+                }
+
+                p {
+                    font-size: 1.7rem;
+                    margin: 1.5rem 0;
+                }
+
+                button {
+                    background-color: $teal;
+                    color: $white;
+                    margin: 1.5rem 0;
+                    font-size: 1.2rem;
+                    padding: 1rem 3rem;
+                }
+            }
+
+            div.office-hours {
+                width: 30%;
+                background-color: #3bb0bf80;
+                padding: 1rem;
+                text-align: center;
+
+                span {
+                    text-transform: uppercase;
+                    display: inline-block;
+                    padding: 1.5rem;
+                    font-size: 1.4rem;
+                }
+
+                hr {
+                    border: 2px solid $teal;
+                    margin-bottom: 1rem;
+                }
+
+                table tr td {
+                    padding: 1.5rem 1.9rem 1.5rem;
+                    text-align: start;
+                }
+            }
+        }
     }
 </style>
