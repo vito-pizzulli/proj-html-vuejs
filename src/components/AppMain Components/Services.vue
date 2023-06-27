0fc48c3e32ad4a4784b652364a8368bfd78376e1
vito-pizzulli <vito.pizzulli.webdev@outlook.it>
2023-06-27 10:05:41 +0200

    imported ServiceCard.vue in Services.vue, created services objects array  in Services.vue

--- src/components/AppMain Components/Services.vue
+++ src/components/AppMain Components/Services.vue
@@ -8,15 +8,65 @@
             <hr>
             <p>Sed Ut Perspiciatis Unde Omnis Iste Natus Error Sit Voluptatem Accusantium Doloremque Laudantium, Totam Rem Aperiam, Eaque Ipsa Quae Ab Illo Inventore Veritatis Et Quasi Architecto Beatae</p>
         </div>
+
+        <!-- Bottom services section, populated by ServiceCard Components -->
+        <div class="services-list">
+            <ServiceCard v-for="service in services"
+                :service="service"
+            />
+        </div>
     </div>
 </template>
+
 <script>
+import ServiceCard from './ServiceCard.vue'
 export default {
-    name: 'Services'
+    name: 'Services',
+
+    components: {
+        ServiceCard
+    },
+
+    data() {
+        return {
+            services: [
+                {
+                    title: 'Rehabilitation Center',
+                    icon: './src/assets/img/icon-14.png',
+                    text: 'At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas'
+                },
+                {
+                    title: 'Dental Implants',
+                    icon: './src/assets/img/icon-15.png',
+                    text: 'At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas'
+                },
+                {
+                    title: 'Medicine Research',
+                    icon: './src/assets/img/icon-16.png',
+                    text: 'At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas'
+                },
+                {
+                    title: 'Blood Bank',
+                    icon: './src/assets/img/icon-11.png',
+                    text: 'At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas'
+                },
+                {
+                    title: 'Pharmaceutical Advice',
+                    icon: './src/assets/img/icon-12.png',
+                    text: 'At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas'
+                },
+                {
+                    title: 'Medical Counseling',
+                    icon: './src/assets/img/icon-13.png',
+                    text: 'At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas'
+                }
+            ]
+        }
+    }
 }
 </script>
+
 <style lang="scss" scoped>
-    @use '../../styles/partials/variables' as *;
     @use '../../styles/partials/mixins' as *;
 
     div#services {
