317b6ee281834f52502b9d7b2e0ea570d901b0e9
vito-pizzulli <vito.pizzulli.webdev@outlook.it>
2023-06-27 12:58:58 +0200

    created ClientCarousel.vue & ClientLogo.vue components, imported into AppMain

---
+++ src/components/AppMain Components/ClientLogo.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="client-logo">
+        <h1>logo</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'ClientLogo'
+}
+</script>
+
+<style lang="">
+    
+</style>
--- src/components/AppMain Components/ClientsCarousel.vue
+++ src/components/AppMain Components/ClientsCarousel.vue
@@ -1,18 +1,23 @@
 <template>
-    <div id="clients">
-        
+    <div id="clients-carousel">
+        <ClientLogo />
     </div>
 </template>
 
 <script>
+import ClientLogo from './ClientLogo.vue'
 export default {
-    name: 'Clients'
+    name: 'Clients',
+
+    components: {
+        ClientLogo
+    }
 }
 </script>
 
 <style lang="scss" scoped>
     
-    div#clients {
+    div#clients-carousel {
         background-image: url(../../assets/img/rgb-gradient-bar-for-logo-carrasel.jpg);
         height: 300px;
     }
