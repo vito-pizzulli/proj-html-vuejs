53ac03ba3b8caeb01a84f74c870f3b84b1295cd6
vito-pizzulli <vito.pizzulli.webdev@outlook.it>
2023-06-27 09:40:58 +0200

    created Services.vue component and imported into AppMain.vue

---
+++ src/components/AppMain Components/Services.vue
@@ -0,0 +1,30 @@
+<template>
+    <div id="services">
+
+        <!-- Upper title section -->
+        <div class="title">
+            <img src="../../assets/img/icon-6.png" alt="Services Icon">
+            <h2>Our Health Services</h2>
+            <hr>
+            <p>Sed Ut Perspiciatis Unde Omnis Iste Natus Error Sit Voluptatem Accusantium Doloremque Laudantium, Totam Rem Aperiam, Eaque Ipsa Quae Ab Illo Inventore Veritatis Et Quasi Architecto Beatae</p>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    name: 'Services'
+}
+</script>
+<style lang="scss" scoped>
+    @use '../../styles/partials/variables' as *;
+    @use '../../styles/partials/mixins' as *;
+
+    div#services {
+        text-align: center;
+        padding: 7rem 0 5rem;
+
+        div.title h2 {
+            text-transform: uppercase;
+        }
+    }
+</style>
--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -3,18 +3,21 @@
 
         <Welcome />
         <Doctors />
+        <Services />
     </main>
 </template>
 
 <script>
 import Welcome from './AppMain Components/Welcome.vue'
 import Doctors from './AppMain Components/Doctors.vue'
+import Services from './AppMain Components/Services.vue'
 export default {
     name: 'AppMain',
 
     components: {
         Welcome,
-        Doctors
+        Doctors,
+        Services
     }
 }
 </script>
