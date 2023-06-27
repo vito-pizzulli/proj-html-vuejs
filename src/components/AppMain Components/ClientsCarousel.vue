0f8241e26d2d9add09ad47c68890588b2ae13238
vito-pizzulli <vito.pizzulli.webdev@outlook.it>
2023-06-27 13:06:25 +0200

    created client logos objects array in ClientsCarousel, passed to ClientLogo with props, styling completed

--- src/components/AppMain Components/ClientsCarousel.vue
+++ src/components/AppMain Components/ClientsCarousel.vue
@@ -1,6 +1,10 @@
 <template>
     <div id="clients-carousel">
-        <ClientLogo />
+        <div class="container">
+            <ClientLogo v-for="client in clients"
+            :client="client"
+        />
+        </div>
     </div>
 </template>
 
@@ -11,14 +15,40 @@ export default {
 
     components: {
         ClientLogo
+    },
+
+    data() {
+        return {
+            clients: [
+                {
+                    logo: './src/assets/img/client-logos-1.png'
+                },
+                {
+                    logo: './src/assets/img/client-logos-2.png'
+                },
+                {
+                    logo: './src/assets/img/client-logos-3.png'
+                },
+                {
+                    logo: './src/assets/img/client-logos-4.png'
+                }
+            ]
+        }
     }
 }
 </script>
 
 <style lang="scss" scoped>
+    @use '../../styles/partials/mixins' as *;
     
     div#clients-carousel {
         background-image: url(../../assets/img/rgb-gradient-bar-for-logo-carrasel.jpg);
-        height: 300px;
+        padding: 2.5rem 0;
+
+        div.container {
+            @include flex(row, center, center, no-wrap);
+            width: 70%;
+            margin: auto;
+        }
     }
 </style>
