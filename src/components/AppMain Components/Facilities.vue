1524753f01d931cb7eae3fe545e59406dc55e85e
vito-pizzulli <vito.pizzulli.webdev@outlook.it>
2023-06-27 10:38:22 +0200

    created title section & objects array with the facilities info in Facilities.vue component

--- src/components/AppMain Components/Facilities.vue
+++ src/components/AppMain Components/Facilities.vue
@@ -1,6 +1,14 @@
 <template>
     <div id="facilities">
-        <h1>Sono Facilities.vue</h1>
+
+        <!-- Upper title section -->
+        <div class="title">
+            <img src="../../assets/img/icon-7.png" alt="Facilities Icon">
+            <h2>Tour Our Facilities</h2>
+            <hr>
+            <p>Sed Ut Perspiciatis Unde Omnis Iste Natus Error Sit Voluptatem Accusantium Doloremque Laudantium, Totam Rem Aperiam, Eaque Ipsa Quae Ab Illo Inventore Veritatis Et Quasi Architecto Beatae</p>
+            <img src="../../assets/img/play-icon.png" alt="Play Icon">
+        </div>
         <FacilityCard />
     </div>
 </template>
@@ -12,10 +20,44 @@ export default {
 
     components: {
         FacilityCard
+    },
+
+    data() {
+        return {
+            facilities: [
+                {
+                    title: 'Emergency Care',
+                    text: 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque',
+                    link: '#'
+                },
+                {
+                    title: 'Expert Diagnostics',
+                    text: 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque',
+                    link: '#'
+                },
+                {
+                    title: 'Superb Rehabilitation',
+                    text: 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque',
+                    link: '#'
+                }
+            ]
+        }
     }
 }
 </script>
 
 <style lang="scss" scoped>
+    @use '../../styles/partials/mixins' as *;
+    @use '../../styles/partials/variables' as *;
     
+    div#facilities {
+        text-align: center;
+        padding: 7rem 0 5rem;
+        background-image: url('../../assets/img/paralax-bg-tour-facilities.jpg');
+        color: white;
+
+        div.title p {
+            margin-bottom: 1.5rem;
+        }
+    }
 </style>
